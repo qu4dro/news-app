4af1dd2be8f9894fad402c4896f7059846075d9f
unknown <qu4dro.sl@gmail.com>
2021-03-30 11:07:49 +0800

    Project dependencies, structure and permissions added

--- app/src/main/java/ru/orlovvv/yandex/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/ru/orlovvv/yandex/newsapp/ui/NewsActivity.kt
@@ -1,11 +1,12 @@
-package ru.orlovvv.yandex.newsapp
+package ru.orlovvv.yandex.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.orlovvv.yandex.newsapp.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }

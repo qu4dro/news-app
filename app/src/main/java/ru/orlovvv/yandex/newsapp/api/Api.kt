ff1a13ded40cd4f5f9fc77aeccfb7fe1aa3e61f1
unknown <qu4dro.sl@gmail.com>
2021-03-30 14:42:11 +0800

    Api getTopNews fun added (return type required)

--- app/src/main/java/ru/orlovvv/yandex/newsapp/api/Api.kt
+++ app/src/main/java/ru/orlovvv/yandex/newsapp/api/Api.kt
@@ -1,4 +1,17 @@
 package ru.orlovvv.yandex.newsapp.api
 
-class Api {
+import retrofit2.http.GET
+import retrofit2.http.Query
+import ru.orlovvv.yandex.newsapp.util.ApiKey.Companion.DEVELOPER_API_KEY
+
+interface Api {
+
+    @GET("v2/top-headlines")
+    suspend fun getTopNews(
+        @Query("country") country: String = "ru",
+        @Query("page") page: Int = 1,
+        @Query("pageSize") pageSize: Int = 20,
+        @Query("apiKey") apiKey: String = DEVELOPER_API_KEY
+        )
+
 }

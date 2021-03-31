9008947bfd6d2110a5ee6eeea00e01d8a090d38b
unknown <qu4dro.sl@gmail.com>
2021-03-31 11:26:27 +0800

    News view model and provider factory added

---
+++ app/src/main/java/ru/orlovvv/yandex/newsapp/ui/NewsViewModel.kt
@@ -0,0 +1,9 @@
+package ru.orlovvv.yandex.newsapp.ui
+
+import androidx.lifecycle.ViewModel
+import ru.orlovvv.yandex.newsapp.repository.NewsRepository
+
+class NewsViewModel(val newsRepository: NewsRepository) : ViewModel(){
+
+
+}
---
+++ app/src/main/java/ru/orlovvv/yandex/newsapp/ui/NewsViewModelProviderFactory.kt
@@ -0,0 +1,11 @@
+package ru.orlovvv.yandex.newsapp.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ru.orlovvv.yandex.newsapp.repository.NewsRepository
+
+class NewsViewModelProviderFactory(val newsRepository: NewsRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return NewsViewModel(newsRepository) as T
+    }
+}

d709d60507fa311be3bd8278276be99ba86c3a42
unknown <qu4dro.sl@gmail.com>
2021-04-01 16:41:38 +0800

    Search bar reworked

--- app/src/main/java/ru/orlovvv/peter/newsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -1,7 +1,66 @@
 package ru.orlovvv.peter.newsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_search_news.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import ru.orlovvv.peter.newsapp.R
+import ru.orlovvv.peter.newsapp.adapters.NewsAdapter
+import ru.orlovvv.peter.newsapp.databinding.FragmentNewsFeedBinding
+import ru.orlovvv.peter.newsapp.databinding.FragmentSearchNewsBinding
+import ru.orlovvv.peter.newsapp.ui.NewsActivity
+import ru.orlovvv.peter.newsapp.ui.NewsViewModel
 
 class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+
+    private lateinit var newsViewModel: NewsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        newsViewModel = (activity as NewsActivity).newsViewModel
+
+        val binding = FragmentSearchNewsBinding.inflate(inflater)
+
+        binding.apply {
+            lifecycleOwner = this@SearchNewsFragment
+            viewModel = newsViewModel
+
+        }
+
+
+//        var job: Job? = null
+//        sv_search_news.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                return true
+//            }
+//
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                if (newText != null) {
+//                    newsViewModel.findNews(newText)
+//                }
+//                return true
+//            }
+//
+//        })
+//            job?.cancel()
+//            job = MainScope().launch {
+//
+//                newsViewModel.findNews(sv_search_news.query.toString())
+//            }
+//        }
+
+
+        return binding.root
+    }
+
 }

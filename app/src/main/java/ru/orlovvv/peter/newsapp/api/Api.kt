9e4e6169d24260d51e83f09080f6e99b5a96e2f6
unknown <qu4dro.sl@gmail.com>
2021-04-01 14:50:44 +0800

    Added findNews and getTopNews fun

--- app/src/main/java/ru/orlovvv/peter/newsapp/api/Api.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/api/Api.kt
@@ -4,9 +4,19 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 import ru.orlovvv.peter.newsapp.models.news.NewsResponse
+import ru.orlovvv.peter.newsapp.models.news_sources.SourceResponse
 import ru.orlovvv.peter.newsapp.util.ApiKey.Companion.DEVELOPER_API_KEY
 
 interface Api {
+
+    @GET("/v2/everything")
+    suspend fun findNews(
+        @Query("q") searchQuery: String,
+        @Query("page") pageNumber: Int = 1,
+        @Query("pageSize") pageSize: Int = 20,
+        @Query("apiKey") apiKey: String = DEVELOPER_API_KEY
+    ): Response<NewsResponse>
+
 
     @GET("v2/top-headlines")
     suspend fun getTopNews(
@@ -16,5 +26,10 @@ interface Api {
         @Query("apiKey") apiKey: String = DEVELOPER_API_KEY
     ): Response<NewsResponse>
 
-//    @GET("v2/everything")
+    @GET("/v2/sources")
+    suspend fun getAllSources(
+        @Query("language") language: String = "ru",
+        @Query("country") country: String = "ru",
+        @Query("apiKey") apiKey: String = DEVELOPER_API_KEY
+    ) : Response<SourceResponse>
 }

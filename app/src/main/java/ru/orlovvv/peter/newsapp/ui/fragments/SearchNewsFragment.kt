973c47d32a83205b0c0b1d916051750055a7f4ae
unknown <qu4dro.sl@gmail.com>
2021-04-01 10:36:12 +0800

    Search fragment added (without logic)

---
+++ app/src/main/java/ru/orlovvv/peter/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,7 @@
+package ru.orlovvv.peter.newsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import ru.orlovvv.peter.newsapp.R
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+}

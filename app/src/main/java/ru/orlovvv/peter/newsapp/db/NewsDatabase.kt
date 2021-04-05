f7935442222b96d22697c409c56e5bb602d00af9
unknown <qu4dro.sl@gmail.com>
2021-04-05 11:41:31 +0800

    Database created

--- app/src/main/java/ru/orlovvv/peter/newsapp/db/NewsDatabase.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/db/NewsDatabase.kt
@@ -1,4 +1,32 @@
 package ru.orlovvv.peter.newsapp.db
 
-class NewsDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import ru.orlovvv.peter.newsapp.models.news.Article
+
+@Database(entities = [Article::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class NewsDatabase : RoomDatabase() {
+
+    abstract fun getNewsDao(): NewsDAO
+
+    companion object {
+
+        @Volatile
+        private var instance: NewsDatabase? = null
+        private var LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            NewsDatabase::class.java,
+            "news_db.db"
+        ).build()
+    }
 }

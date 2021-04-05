d70f0ac2954e15a34052446b9e3a6be2e97b1f16
unknown <qu4dro.sl@gmail.com>
2021-04-05 11:45:29 +0800

    Updated with db methods

--- app/src/main/java/ru/orlovvv/peter/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/repository/NewsRepository.kt
@@ -1,11 +1,19 @@
 package ru.orlovvv.peter.newsapp.repository
 
 import ru.orlovvv.peter.newsapp.api.RetrofitInstance
+import ru.orlovvv.peter.newsapp.db.NewsDatabase
+import ru.orlovvv.peter.newsapp.models.news.Article
 
-class NewsRepository {
+class NewsRepository(val newsDB: NewsDatabase) {
 
+    // network
     suspend fun getTopNews() = RetrofitInstance.api.getTopNews()
     suspend fun getAllSources() = RetrofitInstance.api.getAllSources()
     suspend fun findNews(searchQuery: String) = RetrofitInstance.api.findNews(searchQuery)
+
+    // db
+    suspend fun insert(article: Article) = newsDB.getNewsDao().insert(article)
+    suspend fun delete(article: Article) = newsDB.getNewsDao().delete(article)
+    fun getAll() = newsDB.getNewsDao().getAll()
 
 }

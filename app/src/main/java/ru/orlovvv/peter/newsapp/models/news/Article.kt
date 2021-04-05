89473b00c994a39cf73d7f1c0714886ddaae2ea4
unknown <qu4dro.sl@gmail.com>
2021-04-05 11:33:52 +0800

    Updated with table

--- app/src/main/java/ru/orlovvv/peter/newsapp/models/news/Article.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/models/news/Article.kt
@@ -1,8 +1,13 @@
 package ru.orlovvv.peter.newsapp.models.news
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.io.Serializable
 
+@Entity(tableName = "articles")
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     val author: String?,
     val content: String?,
     val description: String?,

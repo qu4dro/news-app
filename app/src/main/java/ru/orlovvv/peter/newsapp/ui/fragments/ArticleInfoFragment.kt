0665e21f7687948b262dd780e8d053cb6d98f548
unknown <qu4dro.sl@gmail.com>
2021-04-05 10:58:04 +0800

    Article info implementation added

--- app/src/main/java/ru/orlovvv/peter/newsapp/ui/fragments/ArticleInfoFragment.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/ui/fragments/ArticleInfoFragment.kt
@@ -1,7 +1,34 @@
 package ru.orlovvv.peter.newsapp.ui.fragments
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import androidx.navigation.navArgs
 import ru.orlovvv.peter.newsapp.R
+import ru.orlovvv.peter.newsapp.databinding.FragmentArticleInfoBinding
 
 class ArticleInfoFragment : Fragment(R.layout.fragment_article_info) {
+
+    val args: ArticleInfoFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val article = args.article
+        val binding = FragmentArticleInfoBinding.inflate(inflater)
+
+        binding.wvArticleInfo.apply {
+            webViewClient = WebViewClient()
+            article.url?.let { loadUrl(it) }
+        }
+        return binding.root
+    }
 }

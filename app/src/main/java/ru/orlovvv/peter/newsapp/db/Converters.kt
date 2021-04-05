616cf2b0258fedbc5b62d4af1bf37ad6bcfbf9ef
unknown <qu4dro.sl@gmail.com>
2021-04-05 11:27:38 +0800

    Converters created

--- app/src/main/java/ru/orlovvv/peter/newsapp/db/Converters.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/db/Converters.kt
@@ -1,4 +1,18 @@
 package ru.orlovvv.peter.newsapp.db
 
+import androidx.room.TypeConverter
+import ru.orlovvv.peter.newsapp.models.news.Source
+import ru.orlovvv.peter.newsapp.models.news_sources.SourceResponse
+
 class Converters {
+
+    @TypeConverter
+    fun fromSource(source: Source): String? {
+        return source.name
+    }
+
+    @TypeConverter
+    fun toSource(name: String): Source {
+        return Source(name, name)
+    }
 }

5112b8acc64ab5af1ace1536c6c5dd4c173dc5d1
unknown <qu4dro.sl@gmail.com>
2021-04-05 11:34:22 +0800

    DAO created

--- app/src/main/java/ru/orlovvv/peter/newsapp/db/NewsDAO.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/db/NewsDAO.kt
@@ -1,4 +1,18 @@
 package ru.orlovvv.peter.newsapp.db
 
-class NewsDAO {
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import ru.orlovvv.peter.newsapp.models.news.Article
+
+@Dao
+interface NewsDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(article: Article): Long
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+
+    @Query(value = "SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
 }

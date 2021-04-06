48dbd97179e2e154966076d3069eb6b7ac9cf813
unknown <qu4dro.sl@gmail.com>
2021-04-06 11:51:27 +0800

    Page size const added

--- app/src/main/java/ru/orlovvv/peter/newsapp/api/Api.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/api/Api.kt
@@ -7,6 +7,7 @@ import ru.orlovvv.peter.newsapp.models.news.NewsResponse
 import ru.orlovvv.peter.newsapp.models.news_sources.SourceResponse
 import ru.orlovvv.peter.newsapp.util.ApiKey.Companion.DEVELOPER_API_KEY
 import ru.orlovvv.peter.newsapp.util.ApiKey.Companion.DEVELOPER_API_KEY2
+import ru.orlovvv.peter.newsapp.util.Constants.Companion.PAGE_SIZE
 
 interface Api {
 
@@ -22,8 +23,8 @@ interface Api {
     @GET("v2/top-headlines")
     suspend fun getTopNews(
         @Query("page") page: Int,
-        @Query("country") country: String = "ru",
-        @Query("pageSize") pageSize: Int = 20,
+        @Query("country") country: String = "us",
+        @Query("pageSize") pageSize: Int = PAGE_SIZE,
         @Query("apiKey") apiKey: String = DEVELOPER_API_KEY2
     ): Response<NewsResponse>
 

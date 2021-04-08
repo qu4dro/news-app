b017398e60ab88b254c2f3453188497ff83374f9
unknown <qu4dro.sl@gmail.com>
2021-04-08 13:09:53 +0800

    Image loading animation added

--- app/src/main/java/ru/orlovvv/peter/newsapp/adapters/BindingAdapters.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/adapters/BindingAdapters.kt
@@ -5,6 +5,9 @@ import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.request.RequestOptions
+import ru.orlovvv.peter.newsapp.R
 import ru.orlovvv.peter.newsapp.adapters.NewsAdapter
 import ru.orlovvv.peter.newsapp.models.news.Article
 
@@ -22,10 +25,12 @@ fun bindImage(imgView: ImageView, urlToImage: String?) {
             urlToImage.toUri().buildUpon().scheme("https").build()
         Glide.with(imgView.context)
             .load(imgUri)
-//            .apply(
-//                RequestOptions()
-//                .placeholder(R.drawable.loading_animation)
-//                .error(R.drawable.ic_broken_image))
+            .format(DecodeFormat.PREFER_RGB_565)
+            .centerInside()
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image))
             .into(imgView)
     }
 }

0f10b94cad50ba1dfe352d6d902bb7d301a1eccd
unknown <qu4dro.sl@gmail.com>
2021-04-12 16:08:29 +0800

    Updated with item click listener and save logic implementation

--- app/src/main/java/ru/orlovvv/peter/newsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/adapters/NewsAdapter.kt
@@ -1,5 +1,8 @@
 package ru.orlovvv.peter.newsapp.adapters
 
+import android.app.ActionBar
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -7,12 +10,14 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_article.*
 import kotlinx.android.synthetic.main.item_article.view.*
+import ru.orlovvv.peter.newsapp.R
 import ru.orlovvv.peter.newsapp.databinding.ItemArticleBinding
 import ru.orlovvv.peter.newsapp.models.news.Article
 import ru.orlovvv.peter.newsapp.ui.NewsViewModel
 
-class NewsAdapter(var viewModel: NewsViewModel) :
+class NewsAdapter(private var viewModel: NewsViewModel) :
     ListAdapter<Article, NewsAdapter.NewsViewHolder>(NewsCallBack()) {
 
     class NewsViewHolder(private val binding: ItemArticleBinding) :
@@ -47,46 +52,38 @@ class NewsAdapter(var viewModel: NewsViewModel) :
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         val article = getItem(position)
         holder.itemView.setOnClickListener {
-            onItemClickListener?.let {
+            if (it.cl_expand_information.visibility == View.GONE) {
+                TransitionManager.beginDelayedTransition(holder.itemView.cv_article, AutoTransition())
+                it.cl_expand_information.visibility = View.VISIBLE;
+            } else {
+                TransitionManager.beginDelayedTransition(holder.itemView.cv_article, AutoTransition())
+                it.cl_expand_information.visibility = View.GONE;
+            }
+        }
+        holder.itemView.btn_save.setOnClickListener {
+            viewModel.saveToReadLater(article)
+        }
+
+        holder.itemView.btn_source.setOnClickListener {
+            onSourceClickListener?.let {
                 it(article)
             }
         }
-        holder.itemView.tv_save.setOnClickListener {
-            viewModel.saveToReadLater(article)
-            holder.itemView.cv_article.isChecked = false
-            hideSaveAndShareButtons(holder, holder.itemView.cv_article.isChecked)
-        }
-        holder.itemView.cv_article.apply {
-            setOnLongClickListener {
-                isChecked = !isChecked
-                hideSaveAndShareButtons(holder, isChecked)
-                true
-            }
 
+        holder.itemView.btn_share.setOnClickListener {
+            //TODO SHARE
         }
+
         holder.bind(article)
     }
 
-    private var onItemClickListener: ((Article) -> Unit)? = null
+    private var onSourceClickListener: ((Article) -> Unit)? = null
 
     fun setOnItemClickListener(listener: (Article) -> Unit) {
-        onItemClickListener = listener
+        onSourceClickListener = listener
     }
 
     fun getItemWithPosition(position: Int): Article {
         return getItem(position)
-    }
-
-    fun hideSaveAndShareButtons(holder: NewsViewHolder, isChecked: Boolean) {
-        holder.itemView.apply {
-            if (isChecked) {
-                tv_save.visibility = View.VISIBLE
-                tv_share.visibility = View.VISIBLE
-            } else {
-                tv_save.visibility = View.GONE
-                tv_share.visibility = View.GONE
-            }
-        }
-
     }
 }

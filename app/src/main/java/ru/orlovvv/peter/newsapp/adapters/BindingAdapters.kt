7b3b42c63f9f8bb3a38b0e8317e65925c90b5c09
unknown <qu4dro.sl@gmail.com>
2021-04-13 11:53:45 +0800

    Added adapter for chipgroup with popular sources list

--- app/src/main/java/ru/orlovvv/peter/newsapp/adapters/BindingAdapters.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/adapters/BindingAdapters.kt
@@ -1,15 +1,20 @@
 package ru.orlovvv.peter.newsapp.util
 
+import android.content.Context
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import ru.orlovvv.peter.newsapp.R
 import ru.orlovvv.peter.newsapp.adapters.NewsAdapter
 import ru.orlovvv.peter.newsapp.models.news.Article
+import ru.orlovvv.peter.newsapp.models.news_sources.NewsSourceInfo
 
 @BindingAdapter("listData")
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<Article>?) {
@@ -31,6 +36,20 @@ fun bindImage(imgView: ImageView, urlToImage: String?) {
     }
 }
 
+
+@BindingAdapter("items")
+fun setItems(view: ChipGroup, sources: LiveData<List<NewsSourceInfo>>?) {
+    if (sources == null
+        || view.childCount > 0
+    ) return
+
+    val context: Context = view.context
+    for (source in sources.value!!) {
+        val chip = Chip(context)
+        chip.text = source.name
+        view.addView(chip)
+    }
+}
 //@BindingAdapter("imageUrl")
 //fun setImageUrl(imageView: ImageView, url: String) {
 //    Glide.with(imageView.context).load(url).into(imageView)

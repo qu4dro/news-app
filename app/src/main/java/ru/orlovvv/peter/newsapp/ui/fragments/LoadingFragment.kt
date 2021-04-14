756b1a140016df35ebf65f61b8d94cc7acaa0538
unknown <qu4dro.sl@gmail.com>
2021-04-14 16:17:08 +0800

    Added loading fragment

---
+++ app/src/main/java/ru/orlovvv/peter/newsapp/ui/fragments/LoadingFragment.kt
@@ -0,0 +1,16 @@
+package ru.orlovvv.peter.newsapp.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_news.*
+import ru.orlovvv.peter.newsapp.R
+
+class LoadingFragment : Fragment(R.layout.fragment_loading) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}

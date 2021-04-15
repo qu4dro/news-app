01012aa667b254b64645f10c29b10ee385cc5dfd
unknown <qu4dro.sl@gmail.com>
2021-04-16 00:43:49 +0800

    Primary key wrong key fixed

--- app/src/main/java/ru/orlovvv/peter/newsapp/models/news/Article.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/models/news/Article.kt
@@ -21,9 +21,9 @@ data class Article(
     val publishedAt: String?,
     val source: Source?,
     val title: String?,
-    val url: String?,
     @PrimaryKey
-    val urlToImage: String
+    val url: String,
+    val urlToImage: String?
 ) : Serializable {
 
     val dateFormatted: String

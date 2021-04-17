f97107188da6f4af690ed519d2d0a7afe584481d
unknown <qu4dro.sl@gmail.com>
2021-04-18 02:03:32 +0800

    Added click listeners to save and share

--- app/src/main/java/ru/orlovvv/peter/newsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -1,5 +1,6 @@
 package ru.orlovvv.peter.newsapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_news.*
 import kotlinx.android.synthetic.main.fragment_search_news.*
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.MainScope
@@ -16,6 +19,7 @@ import kotlinx.coroutines.launch
 import ru.orlovvv.peter.newsapp.R
 import ru.orlovvv.peter.newsapp.adapters.NewsAdapter
 import ru.orlovvv.peter.newsapp.databinding.FragmentSearchNewsBinding
+import ru.orlovvv.peter.newsapp.models.news.Article
 import ru.orlovvv.peter.newsapp.ui.NewsActivity
 import ru.orlovvv.peter.newsapp.ui.NewsViewModel
 import ru.orlovvv.peter.newsapp.util.Pagination
@@ -37,15 +41,6 @@ class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
         newsViewModel = (activity as NewsActivity).newsViewModel
 
         newsFeedAdapter = NewsAdapter(newsViewModel)
-        newsFeedAdapter.setOnSourceClickListener {
-            val bundle = Bundle().apply {
-                putSerializable("article", it)
-            }
-            findNavController().navigate(
-                R.id.action_searchNewsFragment_to_articleInfoFragment,
-                bundle
-            )
-        }
 
         binding.apply {
             lifecycleOwner = this@SearchNewsFragment
@@ -68,8 +63,14 @@ class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
             })
         }
 
-        val scrollListener = Pagination(newsViewModel, newsViewModel.searchedNewsArticlesList
-        ) { newsViewModel.findNews(binding.etSearchNews.text.toString(), newsViewModel.checkedSources) }.scrollListener
+        val scrollListener = Pagination(
+            newsViewModel, newsViewModel.searchedNewsArticlesList
+        ) {
+            newsViewModel.findNews(
+                binding.etSearchNews.text.toString(),
+                newsViewModel.checkedSources
+            )
+        }.scrollListener
 
         binding.rvSearchedNews.addOnScrollListener(scrollListener)
 
@@ -89,4 +90,35 @@ class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        newsFeedAdapter.apply {
+            setOnSaveClickListener { article ->
+                newsViewModel.saveToReadLater(article)
+                Snackbar.make(view, "Article saved", Snackbar.LENGTH_LONG)
+                    .setAction("Undo") {
+                        newsViewModel.saveToReadLater(article)
+                    }
+                    .setAnchorView((activity as NewsActivity).bn_menu)
+                    .show()
+            }
+
+            setOnShareClickListener {
+                val intent = Intent(Intent.ACTION_SEND).setType("text/plain")
+                    .putExtra(Intent.EXTRA_TEXT, it)
+                startActivity(intent)
+            }
+
+            setOnSourceClickListener {
+                val bundle = Bundle().apply {
+                    putSerializable("article", it)
+                }
+                findNavController().navigate(
+                    R.id.action_searchNewsFragment_to_articleInfoFragment,
+                    bundle
+                )
+            }
+        }
+    }
 }

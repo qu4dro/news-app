66ce301329c89db5365b39a8cfdac4e5c7f10882
unknown <qu4dro.sl@gmail.com>
2021-06-12 01:10:33 +0800

    Fixed date format

--- app/src/main/java/ru/orlovvv/peter/newsapp/util/Extensions.kt
+++ app/src/main/java/ru/orlovvv/peter/newsapp/util/Extensions.kt
@@ -6,11 +6,9 @@ import java.util.*
 
 fun String.formatDate(): String? {
     val pattern: String = "yyyy-MM-dd'T'HH:mm:ss"
-    val outputFormat: DateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH)
+    val outputFormat: DateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH)
     val inputFormat: DateFormat = SimpleDateFormat(pattern, Locale.ENGLISH)
-
-    val inputText = "2012-11-17T00:00:00.000-05:00"
-    val date: Date = inputFormat.parse(inputText)
+    val date: Date = inputFormat.parse(this)
     return outputFormat.format(date)
 
 }
